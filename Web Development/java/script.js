c7396b6d80b3f1317e6f43fe078e98e7cf74961d
Emeika <shahbazhafsa2014@gmail.com>
2023-11-17 22:04:20 +0500

    react

---
+++ Web Development/java/script.js
@@ -0,0 +1,24 @@
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log(i);
+
+for (let i = 0; i < 38; i++) {
+    console.log(i);
+}
+console.log(i);
+
+
+// ES6 JavaScript code with template literals
+const name = "John";
+const age = 30;
+
+const message = `Hello, my name is ${name} and I am ${age} years old.`;
+
+console.log(message);
+
+document.body.innerHTML = `<p>${message}</p>`;
+
+const message2 = `helloo ${age}`;
+console.log(message2);
+
