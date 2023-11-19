6ba81a1bc3fd8b37f81d1f966421c36046e677f0
Emeika <shahbazhafsa2014@gmail.com>
2023-11-19 16:13:52 +0500

    react fcc app

---
+++ Web Development/my-app/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react" 
+ 
+export default function Main() { 
+    return ( 
+        <main> 
+            <h1 className="main--title">Fun facts about FCCU</h1> 
+            <ul className="main--facts"> 
+                <li>Fact 1</li> 
+                <li>Fact 2</li> 
+                <li>Fact 3</li> 
+                <li>Fact 4</li> 
+                <li>Fact 5</li> 
+            </ul> 
+        </main> 
+    ) 
+}
---
+++ Web Development/my-app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (  
+        <nav> 
+            <img src="logo.png" className="nav--icon" /> 
+            <h3 className="nav--logo_text">FCCU</h3> 
+            <h4 className="nav--title">Lahore - 54600</h4> 
+        </nav> 
+    )
+}
