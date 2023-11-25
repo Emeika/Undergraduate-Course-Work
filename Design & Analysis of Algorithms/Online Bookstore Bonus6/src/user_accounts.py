7b13cebc4abd9d5d239b0eb5358cc4bbb43bea19
Emeika <shahbazhafsa2014@gmail.com>
2023-11-25 23:33:00 +0500

    module: add, get user details

---
+++ Design & Analysis of Algorithms/Online Bookstore Bonus6/src/user_accounts.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.purchase_history = []
+
+class UserAccounts:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self, user):
+        self.users[user.username] = user
+
+    def get_user_details(self, username):
+        return self.users.get(username, None)
