4315d4de54a7c4f5a21db710e61981b9ce8f3902
Emeika <shahbazhafsa2014@gmail.com>
2023-11-28 20:42:39 +0500

    navbar

---
+++ Web Development/lab6/src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <header className="navbar">
+        <h1>My Travel Journal</h1>
+        <nav>
+            <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            </ul>
+        </nav>
+        </header>
+    );
+};
+
+export default Navbar;
