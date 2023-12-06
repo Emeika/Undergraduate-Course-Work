8bf27e5d2b4264cadefc7bd7d98f48d3d7406541
Emeika <shahbazhafsa2014@gmail.com>
2023-12-06 17:51:34 +0500

    Lab-ToDoApp

---
+++ Web Development/lab7ToDoApp/src/App.jsx
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Form from './components/Form';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Form</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
